dff30909aba032debdda8a7dd2a2236bb229f757
gabrieularyssa <gabrielaoliveiracet@gmail.com>
2023-01-21 03:35:00 -0300

    feat(TypeORM/container): settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { BudgetsController } from './budgets/budgets.controller';
@@ -6,7 +7,17 @@ import { AppService } from './app.service';
 import { BudgetsModule } from './budgets/budgets.module';
 
 @Module({
-  imports: [BudgetsModule],
+  imports: [BudgetsModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    password:'docker',
+    database:'postgres',
+    autoLoadEntities: true,
+    synchronize: true,
+  })
+],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/budgets/entities/budget.entity.ts
+++ src/budgets/entities/budget.entity.ts
@@ -1,8 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('budgets')
 export class Budget {
+
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     local: string;
+
+    @Column()
     tamanhoCM: string;
+
+    @Column()
     cor: string;
+
     //string[] isso define um array de string
+    @Column('json', { nullable: true })
     detalhes: string[];
 }
